70dad68004a23d2a1960b2b7e0172be5bdd2bea3
Brian Rue <brian@ratchet.io>
2012-06-15 01:07:40 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os.path
+from setuptools import Command, find_packages, setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+README_PATH = os.path.join(HERE, 'README.rst')
+try:
+    README = open(README_PATH).read()
+except IOError:
+    README = ''
+
+setup(
+    name='ratchet-agent',
+    package_files=['ratchet_agent', 'ratchet-agent-init.sh', 'ratchet-agent.conf'],
+    version='0.1',
+    description='Ratchet.io server-side agent',
+    long_description=README,
+    author='Brian Rue',
+    author_email='brian@ratchet.io',
+    url='http://github.com/brianr/ratchet-agent',
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Bug Tracking",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance",
+        ],
+    install_requires=[
+        'requests',
+        ],
+    )
+
+
+
+
+
+
+

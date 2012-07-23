67c00174f694638126ecde636b4a6b2df962046e
Brian Rue <brian@ratchet.io>
2012-07-23 13:47:50 -0700

    setup.py fixes, use https

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ except IOError:
 
 setup(
     name='ratchet-agent',
-    package_files=['ratchet_agent', 'ratchet-agent-init.sh', 'ratchet-agent.conf'],
-    version='0.1.2',
+    version='0.1.3',
     description='Ratchet.io server-side agent',
     long_description=README,
     author='Brian Rue',

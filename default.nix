7f4c2e80f33626529967b844b52fd0bf7e3a6e45
Zach Collins <recursive.cookie.jar@gmail.com>
2018-01-25 11:34:08 -0800

    Removing nix file

--- default.nix
+++
@@ -1,9 +0,0 @@
-{
-  pkgs ? import <nixpkgs> {},
-  bash ? pkgs.bash,
-  python ? pkgs.python,
-  stdenv ? pkgs.stdenv,
-  pythonPackages ? pkgs.python27Packages }:
-
-
-(python.withPackages (ps: [ps.requests])).env
